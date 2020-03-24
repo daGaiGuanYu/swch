c65b838516352ffab134850bbbba932bc4ff9bd5
关于 <372301467@qq.com>
2020-03-25 03:03:36 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 // 如果 fn 是 async 函数，则这里会被认为是普通函数，并且返回一个 Promise 实例，并不会改变原来的特性
 
-module.exports = function(...list){
-  let fn = list.pop()
-  
+module.exports = function(list, fn){
   let glove
   while(glove = list.pop())
     fn = wear(fn, glove)
